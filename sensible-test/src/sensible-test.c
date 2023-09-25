2e418a65c5e4ea96dc1d96c629b1b51ab052582c
Owen Shepherd <owen@owen.cafe>
2023-09-25 21:07:42 +0200

    chore: Remove copyright headers

--- sensible-test/src/sensible-test.c
+++ sensible-test/src/sensible-test.c
@@ -1,7 +1,3 @@
-// Copyright 2023 The libsensible Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 #include <assert.h>
 #include <stdarg.h>
 #include <stdbool.h>
