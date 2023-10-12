dc42fe7f43d855f0a67f96f504e43ddf47e16db5
Owen Shepherd <owen@owen.cafe>
2023-10-12 22:49:45 +0200

    bench: Compare allocator to malloc

--- sensible-allocators/sensible-arena/test/bench.c
+++ sensible-allocators/sensible-arena/test/bench.c
@@ -39,27 +39,61 @@ void print_llu_underscored(long long unsigned n) {
 
 
 int main(void) {
-  const unsigned long n = 1000000000;
-  struct senarena arena = senarena_new();
-
   {
-    double start_time = (double)clock()/CLOCKS_PER_SEC;
-    for (unsigned long i = 0; i < n; i++) {
-      volatile int *ints = senarena_alloc_array_of(&arena, int, 2);
-      ints[1] = 0;
+    const unsigned long n = 1000000000;
+    struct senarena arena = senarena_new();
+
+    {
+      double start_time = (double)clock()/CLOCKS_PER_SEC;
+      for (unsigned long i = 0; i < n; i++) {
+        volatile int *ints = senarena_alloc_array_of(&arena, int, 2);
+        ints[1] = 0;
+      }
+      double end_time = (double)clock()/CLOCKS_PER_SEC;
+      fputs("Small allocations per second: ", stdout);
+      print_llu_underscored(n / (end_time - start_time));
+      putchar('\n');
     }
-    double end_time = (double)clock()/CLOCKS_PER_SEC;
-    fputs("Small allocations per second: ", stdout);
-    print_llu_underscored(n / (end_time - start_time));
-    putchar('\n');
+
+    {
+      double start_time = (double)clock()/CLOCKS_PER_SEC;
+      senarena_free(arena);
+      double end_time = (double)clock()/CLOCKS_PER_SEC;
+      fputs("Time to free: ", stdout);
+      print_llu_underscored(end_time * 1000000 - start_time * 1000000);
+      puts("ns");
+    }
   }
 
   {
-    double start_time = (double)clock()/CLOCKS_PER_SEC;
-    senarena_free(arena);
-    double end_time = (double)clock()/CLOCKS_PER_SEC;
-    fputs("Time to free: ", stdout);
-    print_llu_underscored(end_time * 1000000 - start_time * 1000000);
-    puts("ns");
+    const unsigned long n = 100000000;
+    volatile int **ptrs = malloc(sizeof(void*) * n);
+
+    {
+      double start_time = (double)clock()/CLOCKS_PER_SEC;
+      for (unsigned long i = 0; i < n; i++) {
+        volatile int *ints = malloc(sizeof(int) * 2);
+        // This doesn't slow down the benchmark enough to make
+        // it unfair.
+        ptrs[i] = ints;
+        ints[1] = 0;
+      }
+      double end_time = (double)clock()/CLOCKS_PER_SEC;
+      fputs("Small *standard malloc* allocations per second: ", stdout);
+      print_llu_underscored(n / (end_time - start_time));
+      putchar('\n');
+    }
+
+    {
+      double start_time = (double)clock()/CLOCKS_PER_SEC;
+      for (unsigned long i = 0; i < n; i++) {
+        free((void*)ptrs[i]);
+      }
+      double end_time = (double)clock()/CLOCKS_PER_SEC;
+      fputs("Time to free all *standard malloc* ptrs: ", stdout);
+      print_llu_underscored(end_time * 1000000 - start_time * 1000000);
+      puts("ns");
+    }
+    free(ptrs);
   }
 }
