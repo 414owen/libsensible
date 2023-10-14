819f0909fc02728710083b509fadccc9a4f49ed6
Owen Shepherd <owen@owen.cafe>
2023-10-14 15:02:29 +0200

    feat: Sophisticated arena benchmarking using sensible-timing

--- sensible-allocators/sensible-arena/test/bench.c
+++ sensible-allocators/sensible-arena/test/bench.c
@@ -2,208 +2,323 @@
 //
 // SPDX-License-Identifier: Unlicense
 
+#define _XOPEN_SOURCE 500
+
+#include <inttypes.h>
 #include <stdio.h>
 #include <time.h>
 
 #include "../../../sensible-test/src/sensible-test.h"
+#include "../../../sensible-timing/src/sensible-timing.h"
 #include "../src/sensible-arena.h"
 
-/*
-static
-void print_llu_underscored(long long unsigned n) {
-  unsigned long long length = 0;
-  unsigned long long exp = 1;
-  {
-    unsigned long long m = n;
-    while (m > 0) {
-      length++;
-      m /= 10;
-      exp *= 10;
-    }
-  }
-
-  if (length == 0) {
-    length = 1;
-  }
-
-  exp /= 10;
-  for (int i = length; i > 0; i--) {
-    putchar('0' + (n / exp));
-    n %= exp;
-    exp /= 10;
-    if (i > 1 && (i - 1) % 3 == 0) {
-      putchar('_');
-    }
-  }
-}
-*/
+static const double time_threshold_nanos = 2e8; // rounds should take at least 0.2s
+static const int num_iters = 10;
+static const int ints_to_allocate = 2;
 
 struct minmax_d {
-  double min;
-  double max;
+  uint64_t min;
+  uint64_t max;
 };
 
-void minmax_d(struct minmax_d *out, double d) {
-  if (d < out->min)
-    out->min = d;
-  if (d > out->max)
-    out->max= d;
+static
+void minmax_d(struct minmax_d *out, uint64_t nanos) {
+  if (nanos < out->min)
+    out->min = nanos;
+  if (nanos > out->max)
+    out->max = nanos;
 }
 
-int main(void) {
-  struct minmax_d arena_alloc_aggs = {0};
-  struct minmax_d arena_free_aggs = {0};
-  double arena_alloc_throughput = 0;
-  double arena_free_throughput = 0;
-  double time_threshold_seconds = 1.5; // rounds should take at least 1.5s
+static
+double ns_to_s(uint64_t ns) {
+  // three decimal places
+  return (double) ns / 1e9;
+}
 
-  {
-    unsigned long n = 1;
-    puts("Finding out how many arena allocations will take half a second");
+// Find the number of allocations that makes sense per benchmark iteration
+static
+unsigned long determine_arena_alloc_amt(void) {
+  printf("Finding out how many arena allocations will take %.3fs\n", ns_to_s(time_threshold_nanos));
 
-    {
-      double seconds = 0;
-      while (true) {
-        struct senarena a1 = senarena_new();
-        const double start_time = (double)clock()/CLOCKS_PER_SEC;
-        for (unsigned long i = 0; i < n; i++) {
-          volatile int *ints = senarena_alloc_array_of(&a1, int, 2);
-          ints[1] = 0;
-        }
-        const double end_time = (double)clock()/CLOCKS_PER_SEC;
-        seconds = end_time - start_time;
-        senarena_free(a1);
-        if (seconds >= time_threshold_seconds) break;
-        n *= 2;
-      }
-      printf("%lu arena allocations took %fs\n", n, seconds);
+  unsigned long num_allocations = 2 << 10; // 1024
+  uint64_t nanos = 0;
+  while (true) {
+    printf("\rincreasing allocations: %lu", num_allocations);
+    fflush(stdout);
+    struct senarena a1 = senarena_new();
+    const struct seninstant begin = seninstant_now();
+    for (unsigned long i = 0; i < num_allocations; i++) {
+      volatile int *ints = senarena_alloc_array_of(&a1, int, ints_to_allocate);
+      ints[1] = 0;
     }
-
-    for (int j = 0; j < 10; j++) {
-      printf("\r%d/10", j + 1);
-      fflush(stdout);
-      struct senarena arena = senarena_new();
-      {
-        const double start_time = (double)clock()/CLOCKS_PER_SEC;
-        for (unsigned long i = 0; i < n; i++) {
-          volatile int *ints = senarena_alloc_array_of(&arena, int, 2);
-          ints[1] = 0;
-        }
-        const double end_time = (double)clock()/CLOCKS_PER_SEC;
-        const double time = end_time - start_time;
-        if (j == 0) {
-          arena_alloc_aggs.min = time;
-          arena_alloc_aggs.max = time;
-        } else {
-          minmax_d(&arena_alloc_aggs, time);
-        }
-      }
-
-      {
-        double start_time = (double)clock()/CLOCKS_PER_SEC;
-        senarena_free(arena);
-        double end_time = (double)clock()/CLOCKS_PER_SEC;
-        const double time = end_time - start_time;
-        if (j == 0) {
-          arena_free_aggs.min = time;
-          arena_free_aggs.max = time;
-        } else {
-          minmax_d(&arena_free_aggs, time);
-        }
-      }
-    }
-    putchar('\r');
-    printf("Minimum arena allocation time: %fs\n", arena_alloc_aggs.min);
-    printf("Maximum arena allocation time: %fs\n", arena_alloc_aggs.max);
-    printf("Minimum arena free time: %fs\n", arena_free_aggs.min);
-    printf("Maximum arena free time: %fs\n", arena_free_aggs.max);
-    arena_alloc_throughput = n / (arena_alloc_aggs.max * 1000);
-    printf("Maximum arena allocations per ms: %f\n", arena_alloc_throughput);
-    arena_free_throughput = n / (arena_free_aggs.max * 1000);
-    printf("Maximum arena allocation frees per ms: %f\n", arena_free_throughput);
+    nanos = seninstant_subtract(seninstant_now(), begin);
+    senarena_free(a1);
+    if (nanos >= time_threshold_nanos) break;
+    num_allocations *= 2;
   }
 
   putchar('\n');
 
-  struct minmax_d std_alloc_aggs = {0};
-  struct minmax_d std_free_aggs = {0};
-  double std_alloc_throughput = 0;
-  double std_free_throughput = 0;
-  {
-    unsigned long n = 1;
-    puts("Finding out how many malloc allocations will take half a second");
-    volatile int **ptrs = malloc(sizeof(int*) * n);
+  const int reduction_steps = 5;
+  for (int j = 0; j < reduction_steps; j++) {
+    while (true) {
+      const unsigned long m = num_allocations - num_allocations / (1 << (j + 1));
+      printf("\rreducing %d/%d %lu", j + 1, reduction_steps, m);
+      fflush(stdout);
+      struct senarena a1 = senarena_new();
+      const struct seninstant begin = seninstant_now();
+      for (unsigned long i = 0; i < m; i++) {
+        volatile int *ints = senarena_alloc_array_of(&a1, int, ints_to_allocate);
+        ints[1] = 0;
+      }
+      const uint64_t red_nanos = seninstant_subtract(seninstant_now(), begin);
+      senarena_free(a1);
+      if (red_nanos < time_threshold_nanos) {
+        break;
+      }
+      num_allocations = m;
+      nanos = red_nanos;
+    }
+  }
+  putchar('\n');
+  printf("%lu arena allocations took %.3fs\n", num_allocations, ns_to_s(nanos));
+  return num_allocations;
+}
 
-    {
-      double seconds = 0;
-      while (true) {
-        double start_time = (double)clock()/CLOCKS_PER_SEC;
-        for (unsigned long i = 0; i < n; i++) {
-          volatile int *ints = malloc(sizeof(int) * 2);
-          ptrs[i] = ints;
+// Find the number of allocations that makes sense per benchmark iteration
+static
+unsigned long determine_standard_alloc_amt(void) {
+  printf("Finding out how many standard allocations will take %.3fs\n", ns_to_s(time_threshold_nanos));
+
+  unsigned long num_allocations = 2 << 10; // 1024
+  volatile int **ptrs = malloc(sizeof(int*) * num_allocations);
+  uint64_t nanos = 0;
+  while (true) {
+    printf("\rincreasing allocations: %lu", num_allocations);
+    fflush(stdout);
+    const struct seninstant begin = seninstant_now();
+    for (unsigned long i = 0; i < num_allocations; i++) {
+      volatile int *ints = malloc(sizeof(int) * ints_to_allocate);
+      ptrs[i] = ints;
+      ints[1] = 0;
+    }
+    const struct seninstant end = seninstant_now();
+    for (unsigned long i = 0; i < num_allocations; i++) {
+      free((void*) ptrs[i]);
+    }
+    nanos = seninstant_subtract(end, begin);
+    if (nanos >= time_threshold_nanos) break;
+    num_allocations *= 2;
+    ptrs = realloc(ptrs, sizeof(int*) * num_allocations);
+  }
+
+  putchar('\n');
+
+  const int reduction_steps = 5;
+  for (int j = 0; j < reduction_steps; j++) {
+    while (true) {
+      const unsigned long m = num_allocations - num_allocations / (1 << (j + 1));
+      printf("\rreducing %d/%d %lu", j + 1, reduction_steps, m);
+      fflush(stdout);
+      const struct seninstant begin = seninstant_now();
+      for (unsigned long i = 0; i < m; i++) {
+        volatile int *ints = malloc(sizeof(int) * ints_to_allocate);
+        ptrs[i] = ints;
+        ints[1] = 0;
+      }
+      const uint64_t red_nanos = seninstant_subtract(seninstant_now(), begin);
+      for (unsigned long i = 0; i < m; i++) {
+        free((void*) ptrs[i]);
+      }
+      if (red_nanos < time_threshold_nanos) {
+        break;
+      }
+      num_allocations = m;
+      nanos = red_nanos;
+    }
+  }
+  putchar('\n');
+  printf("%lu arena allocations took %.3fs\n", num_allocations, ns_to_s(nanos));
+  free((void*) ptrs);
+  return num_allocations;
+}
+
+static
+void clearln(void) {
+  puts("\33[2K");
+}
+
+int main(void) {
+  double arena_alloc_throughput = 0;
+  double arena_free_throughput = 0;
+  double arena_alloc_reused_throughput = 0;
+  const unsigned long num_arena_allocations = determine_arena_alloc_amt();
+  const unsigned long num_standard_allocations = determine_standard_alloc_amt();
+
+  {
+    struct minmax_d arena_alloc_aggs = {0};
+    struct minmax_d arena_alloc_reused_aggs = {0};
+    struct minmax_d arena_free_aggs = {0};
+
+    puts("\n# Benchmarking arena use");
+    struct senarena arena;
+
+    for (int j = 0; j < num_iters; j++) {
+      printf("\r%d/%d (a)", j + 1, num_iters);
+      fflush(stdout);
+      arena = senarena_new();
+      {
+        const struct seninstant begin = seninstant_now();
+        for (unsigned long i = 0; i < num_arena_allocations; i++) {
+          volatile int *ints = senarena_alloc_array_of(&arena, int, ints_to_allocate);
           ints[1] = 0;
         }
-        double end_time = (double)clock()/CLOCKS_PER_SEC;
-        for (unsigned long i = 0; i < n; i++) {
-          free((void*) ptrs[i]);
+        const struct seninstant end = seninstant_now();
+        const uint64_t nanos = seninstant_subtract(end, begin);
+        if (j == 0) {
+          arena_alloc_aggs.min = nanos;
+          arena_alloc_aggs.max = nanos;
+        } else {
+          minmax_d(&arena_alloc_aggs, nanos);
         }
-        seconds = end_time - start_time;
-        if (seconds >= time_threshold_seconds) break;
-        n *= 2;
-        ptrs = realloc(ptrs, sizeof(int*) * n);
       }
-      printf("%lu malloc allocations took %fs\n", n, seconds);
+
+      if (j == num_iters - 1) {
+        puts("\r# Benchmarking arena reuse");
+
+        for (int k = 0; k < num_iters; k++) {
+          printf("\r%d/%d (r)", k + 1, num_iters);
+          fflush(stdout);
+          senarena_clear(&arena);
+          {
+            const struct seninstant begin = seninstant_now();
+            for (unsigned long i = 0; i < num_arena_allocations; i++) {
+              volatile int *ints = senarena_alloc_array_of(&arena, int, ints_to_allocate);
+              ints[1] = 0;
+            }
+            const struct seninstant end = seninstant_now();
+            const uint64_t nanos = seninstant_subtract(end, begin);
+            if (k == 0) {
+              arena_alloc_reused_aggs.min = nanos;
+              arena_alloc_reused_aggs.max = nanos;
+            } else {
+              minmax_d(&arena_alloc_reused_aggs, nanos);
+            }
+          }
+        }
+      }
+
+      printf("\r%d/%d (f)", j + 1, num_iters);
+      fflush(stdout);
+
+      const struct seninstant begin = seninstant_now();
+      senarena_free(arena);
+      const struct seninstant end = seninstant_now();
+      const uint64_t nanos = seninstant_subtract(end, begin);
+      if (j == 0) {
+        arena_free_aggs.min = nanos;
+        arena_free_aggs.max = nanos;
+      } else {
+        minmax_d(&arena_free_aggs, nanos);
+      }
     }
 
-    for (int j = 0; j < 10; j++) {
-      printf("\r%d/10", j + 1);
+    clearln();
+    {
+      double min_time = ns_to_s(arena_alloc_aggs.min);
+      double max_time = ns_to_s(arena_alloc_aggs.max);
+      printf("Arena allocation time:             %.3fs (min), %.3fs (max)\n", min_time, max_time);
+    }
+    {
+      double min_time = ns_to_s(arena_alloc_reused_aggs.min);
+      double max_time = ns_to_s(arena_alloc_reused_aggs.max);
+      printf("Arena (reused) allocation time:    %.3fs (min), %.3fs (max)\n", min_time, max_time);
+    }
+    {
+      double min_time = ns_to_s(arena_free_aggs.min);
+      double max_time = ns_to_s(arena_free_aggs.max);
+      printf("Arena free time:                   %.3fs (min), %.3fs (max)\n", min_time, max_time);
+    }
+    arena_alloc_throughput = num_arena_allocations / ((double) arena_alloc_aggs.max / 1000);
+    printf("Arena allocations per us:          %.3f\n", arena_alloc_throughput);
+    arena_alloc_reused_throughput = num_arena_allocations / ((double) arena_alloc_reused_aggs.max / 1000);
+    printf("Arena (reused) allocations per us: %.3f\n", arena_alloc_reused_throughput);
+    arena_free_throughput = num_arena_allocations / ((double) arena_free_aggs.max / 1000);
+    printf("Arena allocation frees per us:     %.3f\n", arena_free_throughput);
+  }
+
+  clearln();
+  puts("# Benchmarking malloc use\n");
+
+  double std_alloc_throughput = 0;
+  double std_free_throughput = 0;
+
+  {
+    struct minmax_d std_alloc_aggs = {0};
+    struct minmax_d std_free_aggs = {0};
+
+    volatile int **ptrs = malloc(sizeof(int*) * num_standard_allocations);
+
+    for (int j = 0; j < num_iters; j++) {
+      printf("\r%d/%d (a)", j + 1, num_iters);
+      fflush(stdout);
       {
-        double start_time = (double)clock()/CLOCKS_PER_SEC;
-        for (unsigned long i = 0; i < n; i++) {
-          volatile int *ints = malloc(sizeof(int) * 2);
+        const struct seninstant begin = seninstant_now();
+        for (unsigned long i = 0; i < num_standard_allocations; i++) {
+          volatile int *ints = malloc(sizeof(int) * ints_to_allocate);
           // This doesn't slow down the benchmark enough to make
           // it unfair.
           ptrs[i] = ints;
           ints[1] = 0;
         }
-        double end_time = (double)clock()/CLOCKS_PER_SEC;
-        const double time = end_time - start_time;
+        const struct seninstant end = seninstant_now();
+        const uint64_t nanos = seninstant_subtract(end, begin);
         if (j == 0) {
-          std_alloc_aggs.min = time;
-          std_alloc_aggs.max = time;
+          std_alloc_aggs.min = nanos;
+          std_alloc_aggs.max = nanos;
         } else {
-          minmax_d(&std_alloc_aggs, time);
+          minmax_d(&std_alloc_aggs, nanos);
         }
       }
 
+      printf("\r%d/%d (f)", j + 1, num_iters);
+      fflush(stdout);
+
       {
-        const double start_time = (double)clock()/CLOCKS_PER_SEC;
-        for (unsigned long i = 0; i < n; i++) {
+        const struct seninstant begin = seninstant_now();
+        for (unsigned long i = 0; i < num_standard_allocations; i++) {
           free((void*)ptrs[i]);
         }
-        const double end_time = (double)clock()/CLOCKS_PER_SEC;
-        const double time = end_time - start_time;
+        const struct seninstant end = seninstant_now();
+        const uint64_t nanos = seninstant_subtract(end, begin);
         if (j == 0) {
-          std_free_aggs.min = time;
-          std_free_aggs.max = time;
+          std_free_aggs.min = nanos;
+          std_free_aggs.max = nanos;
         } else {
-          minmax_d(&std_free_aggs, time);
+          minmax_d(&std_free_aggs, nanos);
         }
       }
     }
     free(ptrs);
     putchar('\r');
-    printf("Minimum standard allocation time: %fs\n", std_alloc_aggs.min);
-    printf("Maximum standard allocation time: %fs\n", std_alloc_aggs.max);
-    printf("Minimum standard free time: %fs\n", std_free_aggs.min);
-    printf("Maximum standard free time: %fs\n", std_free_aggs.max);
-    std_alloc_throughput = n / (std_alloc_aggs.max * 1000);
-    printf("Maximum standard allocations per ms: %f\n", std_alloc_throughput);
-    std_free_throughput = n / (std_free_aggs.max * 1000);
-    printf("Maximum standard allocation frees per ms: %f\n", std_free_throughput);
+    {
+      double min_time = ns_to_s(std_alloc_aggs.min);
+      double max_time = ns_to_s(std_alloc_aggs.max);
+      printf("Standard allocation time:         %.3fs (min), %.3fs (max)\n", min_time, max_time);
+    }
+    {
+      double min_time = ns_to_s(std_free_aggs.min);
+      double max_time = ns_to_s(std_free_aggs.max);
+      printf("Standard free time:               %.3fs (min), %.3fs (max)\n", min_time, max_time);
+    }
+    std_alloc_throughput = num_standard_allocations / ((double) std_alloc_aggs.max / 1000);
+    printf("Standard allocations per us:      %.3f\n", std_alloc_throughput);
+    std_free_throughput = num_standard_allocations / ((double) std_free_aggs.max / 1000);
+    printf("Standard allocation frees per us: %.3f\n", std_free_throughput);
   }
 
   putchar('\n');
-  printf("arena_alloc vs malloc speedup: %f\n", arena_alloc_throughput / std_alloc_throughput);
-  printf("arena_free vs free() speedup: %f\n", arena_free_throughput / std_free_throughput);
+  printf("         arena_alloc() vs malloc() speedup: %.3f\n", arena_alloc_throughput / std_alloc_throughput);
+  printf("(reused) arena_alloc() vs malloc() speedup: %.3f\n", arena_alloc_reused_throughput / std_alloc_throughput);
+  printf("          arena_free() vs   free() speedup: %.3f\n", arena_free_throughput / std_free_throughput);
 }
